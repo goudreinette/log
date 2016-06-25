fc7b5e69f9f8ecf6b0a4fabea7e08b7932776bbe
Rein van der Woerd <rwoerd2033@student.landstede.nl>
2016-06-25 10:16:05 +0200

    Initial commit

---
+++ controller.js
@@ -0,0 +1,33 @@
+const {}
+module.exports = {showEntries, showNew, showEdit, saveEntry, updateEntry, deleteEntry}
+
+function showEntries (req, res)
+{
+  console.log()
+}
+
+
+function showNew (req, res)
+{
+
+}
+
+function showEdit (req, res)
+{
+
+}
+
+function saveEntry (req, res)
+{
+
+}
+
+function updateEntry (req, res)
+{
+
+}
+
+function deleteEntry (req, res)
+{
+
+}
